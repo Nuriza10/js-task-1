6c395d5c3538d8e2faccb33fc5857fd85410b9fc
Nuriza10 <nuriza.abyshkaeva.on@gmail.com>
2022-01-27 16:57:25 +0600

    add task4.js

---
+++ js/task4.js
@@ -0,0 +1,7 @@
+let numberSouvenirs = prompt("How mach do souvenirs cost?");
+let gramSouvenirs = 75;
+let numberDauble = prompt("How mach do bauble cost?");
+let gramDauble = 112;
+
+let resultProduct = numberSouvenirs * gramSouvenirs + numberDauble * gramDauble;
+console.log('General whole - ${totalWeight} gram!');
